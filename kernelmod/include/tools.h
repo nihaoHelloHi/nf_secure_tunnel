cb8b166bfd365761da077296d0fe6f04d20f2056
nihao <1877365685@qq.com>
2025-06-26 15:40:29 +0800

    注释更新

--- kernelmod/include/tools.h
+++ kernelmod/include/tools.h
@@ -6,7 +6,10 @@
 #define NF_SECURE_TUNNEL_TOOLS_H
 
 #include "dependency.h"
-
+// --------------------------------------------------------------------------------
+// === 获取payload信息 ===
 u8* get_transport_payload(struct sk_buff *skb, u16 *len);
+
+
 
 #endif //NF_SECURE_TUNNEL_TOOLS_H
